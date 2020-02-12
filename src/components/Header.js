202cf6ebeae3d8cf5105371bb9ce1c8ee47ece31
Alex Raymond <Alexraymond711@gmail.com>
2020-02-12 12:52:43 -0800

    add profile pic

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
       <Image
         width="400px"
         height="400px"
-        src={require("../images/programming.svg")}
+        src={require("../images/Profile_Pic.jpg")}
       />
       <div
         style={{
@@ -29,7 +29,7 @@ const Header = () => {
           lineHeight: `.10rem`
         }}
       >
-        <SideText>Hi there, I'm Alex Raymond</SideText>
+        <SideText>Hello. I'm Alex Raymond</SideText>
         <Skill>I'm a Full Stack Software Engineer</Skill>
       </div>
     </Heading>
